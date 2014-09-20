34a9e1d04f7f238787efd5a94287a1e2712d2202
Scott Hale <shalemont@gmail.com>
2014-09-20 13:14:15 -0700

    open dev branch

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zurbymart::Application.config.secret_token = '870031937c90328434eb81db668234ede91d3b3475a6b5c89959da622a85b743a6897483981887d1800bc146a682bb0f45c2c36ad52d483282dd7949631d81f8'
